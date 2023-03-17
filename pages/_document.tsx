2d11af7cafcd4c8ec8ef8ad781c973bfad7e0b34
Boseong Jeon <junbs95@gmail.com>
2023-03-17 21:22:49 +0900

    feat(integration): add adsense script to _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,25 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <Script
+        id="Adsense-id"
+        data-ad-client="ca-pub-6825630715558369"
+        async
+        strategy="beforeInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        onError={(e) => {
+          console.error("Script failed to load", e);
+        }}
+      />
+
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
