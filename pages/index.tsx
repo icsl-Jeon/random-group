2f5bcd55da3aa33770cd007416c071c9d7ef0e66
B.S Felipe Jeon <junbs95@gmail.com>
2023-03-18 17:50:14 +0900

    fix(ui): fix bug for link

--- pages/index.tsx
+++ pages/index.tsx
@@ -219,20 +219,22 @@ export default function Home() {
             <Link href={"/posts/tutorial"}> Read docs</Link>
           </div>
           <div className="inline sm:hidden mr-2 pt-1">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="skyblue"
-              className="w-8 h-8"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
-              />
-            </svg>
+            <Link href={"/posts/tutorial"} passHref>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="skyblue"
+                className="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+                />
+              </svg>
+            </Link>
           </div>
         </div>
 
