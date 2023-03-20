3f605ac37fd6d5261b0d2ffb1480731016c0a514
Boseong Jeon <junbs95@gmail.com>
2023-03-20 19:00:27 +0900

    feat(ui): Add contact me

--- pages/index.tsx
+++ pages/index.tsx
@@ -305,6 +305,35 @@ export default function Home() {
           </div>
         </Accordion>
       </div>
+      <div className=" flex flex-col items-center bg-white ">
+        {" "}
+        <div className="flex flex-col items-center py-2 sm:py-5">
+          <button
+            className="flex items-center"
+            onClick={() => {
+              window.location.href = `mailto:junbs95@gmail.com?subject=${encodeURIComponent(
+                "Hello. I am an user of diverse group"
+              )}`;
+            }}
+          >
+            <span className="mr-2 text-gray-500">Contact me: </span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="skyblue"
+              className="w-8 h-8"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
     </>
   );
 }
